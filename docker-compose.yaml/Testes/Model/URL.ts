6929f3e4130a14de6270f9e91139da196e1ab607
MItchell1970 <133356273+MItchell1970@users.noreply.github.com>
2023-12-04 18:02:00 +0000

    Update URL.ts

--- docker-compose.yaml/Testes/Model/URL.ts
+++ docker-compose.yaml/Testes/Model/URL.ts
@@ -1,14 +1,19 @@
-import { prop, Typegoose } from '@hasezoey/typegoose'
+import { prop, getModelForClass, Ref } from '@typegoose/typegoose';
+import { User } from './User'; 
 
-export class URL extends Typegoose {
-	@prop({ required: true })
-	hash: string
+export class URL {
+  @prop({ required: true })
+  hash!: string;
 
-	@prop({ required: true })
-	originURL: string
+  @prop({ required: true })
+  originURL!: string;
 
-	@prop({ required: true })
-	shortURL: string
+  @prop({ required: true })
+  shortURL!: string;
+
+  
+  @prop({ ref: () => User, required: true })
+  createdBy!: Ref<User>;
 }
 
-export const URLModel = new URL().getModelForClass(URL)
+export const URLModel = getModelForClass(URL);
